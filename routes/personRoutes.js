f4e65176e926bc484c2af9724ed91f8d22b31b55
GiovannCarvalho <giovannyjcarvalho@gmail.com>
2022-12-31 10:24:37 -0300

    Correção no métodp POST

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -14,13 +14,13 @@ router.post('/', async (req, res) => {
     //console.log(name)
 
     try{
-        await Person.create(person)
-
         if (!name) {
             res.status(422).json({error: 'O nome é obrigatório'})
             return
+        } else {
+            await Person.create(person)
+            res.status(201).json({message: 'Pessoa incluída com sucesso!'})
         }
-        res.status(201).json({message: 'Pessoa incluída com sucesso!'})
 
     }catch (error) {
         res.status(500).json({error: error})
